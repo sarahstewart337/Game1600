9bd2caae79622f806671f53223a84e69321c958c
unknown <10732204@CS514-16.ad.uvu.edu>
2016-03-25 10:05:56 -0600

    direction

--- Game final/Assets/script/walking/direction.cs
+++ Game final/Assets/script/walking/direction.cs
@@ -1,100 +1,156 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class direction : MonoBehaviour 
-{
+public class direction : MonoBehaviour
+{  
 	private Rigidbody2D myRigidbody;
+	private Animator myAnimator;
+	
 	[SerializeField]
-	private float movementSpeed;
-
+	private float moventSpeed;
+	private bool attack;
+	private bool slide;
 	private bool facingRight;
-
+	
 	[SerializeField]
-	private Transform[] groundPoints;
-
+	private Transform[] GroundPoints;
+	
 	[SerializeField]
 	private float groundRadius;
-
-	private LayerMask whatisGround;
-
-	private bool isGrounded; 
-
+	
+	private LayerMask whatIsGround;
+	
+	private bool isGrounded;
 	private bool jump;
-	private float  jumpForce;
-
-
-
+	private float JumpForce;
+	
+	
 	// Use this for initialization
-	void Start () 
+	void Start()
 	{
 		facingRight = true;
-		myRigidbody = GetComponent<Rigidbody2D> ();
+		myRigidbody = GetComponent <Rigidbody2D>();
+
+	}
+	
+	void Update()
+	{
+		HandleInput();
 	}
 	
 	// Update is called once per frame
-	void FixedUpdate () 
+	void FixedUpdate()
 	{
-		float horizontal = Input.GetAxis("Horizontal");
-		Debug.Log (horizontal);
-
-		isGounded = IsGrounded ();
-
-		HandleMovement(horizontal);
-
-		flip(horizontal);
+		float horizontal = Input.GetAxis ("Horizontal");
+		
+		isGrounded = IsGrounded();
+		
+		HandleMovement (horizontal);
+		
+		Flip(horizontal);
+		
+		Handleattacks();
+		
+		ResetValues();
+		
 	}
-
+	
+	private void HandleMovement(float horizontal)
+	{
+		if (!myAnimator.GetBool("slide") && !this.myAnimator.GetCurrentAnimatorStateInfo(0).IsTag("Attack"))
+		{
+			myRigidbody.velocity = new Vector2(horizontal * moventSpeed, myRigidbody.velocity.y);
+		}
+		
+		if (isGrounded && jump)
+		{
+			isGrounded = false;
+			myRigidbody.AddForce (new Vector2 (0, JumpForce));
+		}
+		
+		if (slide && !this.myAnimator.GetCurrentAnimatorStateInfo (0).IsName ("slide")) {
+			myAnimator.SetBool ("slide", true);
+		}
+		else if (!this.myAnimator.GetCurrentAnimatorStateInfo(0).IsName("Slide"))
+		{
+			myAnimator.SetBool ("slide", false);
+		}
+		
+		
+		myAnimator.SetFloat("speed", Mathf.Abs(horizontal));
+	}
+	
+	private void Handleattacks()
+	{
+		if (attack && !this.myAnimator.GetCurrentAnimatorStateInfo(0).IsTag("Attack"))
+		{
+			myAnimator.SetTrigger("attack");
+			myRigidbody.velocity = Vector2.zero;
+		}
+	}
+	
 	private void HandleInput()
 	{
-		if (Input.GetKeyDown (KeyCode.Space)) 
+		
+		if (Input.GetKeyDown (KeyCode.Space))
 		{
 			jump = true;
 		}
+		
+		if (Input.GetKeyDown (KeyCode.Mouse0))
+		{
+			attack = true;
+		}
+		if (Input.GetKeyDown (KeyCode.LeftShift))
+		{
+			slide = true;
+			
+		}
+		
 	}
-
-	private void HandleMovement(float horizontal)
-	{
-		myRigidbody.velocity = new Vector2 (horizontal * movementSpeed, myRigidbody.velocity.y); 
 	
-
-	if (isGrounded && jump)
+	
+	
+	private void Flip(float horizontal)
 	{
-		isGrounded = false;
-		myRigidbody.AddForce(new Vector2(0, jumpForce));
-	}
-
-	private void flip(float horizontal)
-	{
-		if (horizontal > 0 && !facingRight || horizontal < 0 && facingRight) 
+		if (horizontal > 0 && !facingRight || horizontal < 0 && facingRight)
 		{
 			facingRight = !facingRight;
-
+			
 			Vector3 theScale = transform.localScale;
-
+			
 			theScale.x *= -1;
-
+			
 			transform.localScale = theScale;
 		}
-
 	}
-	private bool IsGrounded()
+	
+	private void ResetValues()
 	{
-		if (myRigidbody.velocity.y <= 0) 
+		attack = false;
+		slide = false;
+	}
+	
+	
+	
+	private bool IsGrounded()
+		
+	{
+		
+		if (myRigidbody.velocity.y <= 0)
 		{
-			foreach(TRansform point in groundpoints)
+			foreach (Transform point in GroundPoints)
 			{
-				Collider2D[] colliders = Physics2D.OverlapCircleAll(point.position, groundRadius, whatisGround);
-
-				for (int i = 0 < colliders.Length; i++)
+				BoxCollider[] colliders = Physics2D.OverlapCircleAll(point.position, groundRadius, whatIsGround);
+				for (int i = 0; i < colliders.Length; i++)
 				{
-					if (colliders[i].gameObject != gameObject
-					    {
+					if (colliders[i].gameObject != gameObject)
+					{
 						return true;
-
-						}
+					}
 				}
 			}
 		}
-					return false;
+		return false;
 	}
 }
