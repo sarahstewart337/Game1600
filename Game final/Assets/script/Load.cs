a623e5eda48bae8c0f1b717c78f2f2e9e1857fc1
Sarah Stewart <sstewartyankeefan@yahoo.com>
2016-04-12 08:41:33 -0600

    3d

---
+++ Game final/Assets/script/Load.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Load : MonoBehaviour
+{
+
+	public GameObject loadingImage;
+
+	public void LoadScene(int play)
+	{
+		loadingImage.SetActive(true);
+		Application.LoadLevel(play);
+	}
+}
