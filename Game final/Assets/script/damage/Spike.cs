3b25267b3e442ab06f3f566e041a0ed149d58cb4
Sarah Stewart <sstewartyankeefan@yahoo.com>
2016-04-16 18:49:07 -0600

    game

--- Game final/Assets/script/damage/Spike.cs
+++ Game final/Assets/script/damage/Spike.cs
@@ -1,15 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Spike : MonoBehaviour {
+public class Spike : MonoBehaviour
+{
 
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.CompareTag ( "Spike"))
+		{
+			Debug.Log("Hit");
+			Destroy (gameObject);
+		}
+	}
 	// Use this for initialization
 	void Start () {
-	
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
 }
---
+++ Game final/Assets/script/score.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class score : MonoBehaviour {
+
+	public float speed;
+	public Text ScoreText;
+	public Text winText;
+
+	private Rigidbody rb;
+	private int Score;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+		Score = 0;
+		SetScoreText ();
+		winText.text = "";
+	}
+
+
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.CompareTag ( "coin"))
+		{
+			other.gameObject.SetActive (false);
+			Score = Score + 1;
+			SetScoreText ();
+		}
+	}
+
+	void SetScoreText ()
+	{
+		ScoreText.text = "Score: " + Score.ToString ();
+		if (Score >= 10)
+		{
+			winText.text = "You Win!";
+		}
+	}
+}
